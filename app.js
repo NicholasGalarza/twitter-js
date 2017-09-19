be7a382f068241ee04e99a143917d5630b6cae27
NicholasGalarza <nicogala93@gmail.com>
2017-09-19 11:31:21 -0400

    added tweetbank spec

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const nunjucks = require('nunjucks');
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
 const locals = {
   title: 'An Example',
   people: [
@@ -10,21 +14,6 @@ const locals = {
     { name: 'Hermione'}
   ]
 };
-
-nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
-
-nunjucks.render('index.html', locals, function (err, output) {
-  console.log(output);
-});
-
-// nunjucks.configure('views', {
-//   autoescape: true,
-//   express: app
-// });
-
-app.set('view engine', 'html'); // have res.render work with html files
-
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 app.get('/', function(req, res) {
   res.render('index', locals);
@@ -41,11 +30,6 @@ app.use('/special', function (req, res, next) {
   next();
 });
 
-//Routes
-// app.get('/', function(req, res) {
-//   res.send('Home Page: Hello World!');
-// });
-
 app.post('/testing', function(req, res) {
   res.send('POST: Testing');
 });
@@ -53,7 +37,6 @@ app.post('/testing', function(req, res) {
 app.get('/news', function(req, res) {
   res.send('The news will ruin your day');
 });
-
 
 app.listen(3000, function() {
   console.log("Listening on port ", 3000);
