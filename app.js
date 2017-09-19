27e3d198af8b50a516d2b3e0e56c19eec8c6e672
John Pepino <jvpepino@gmail.com>
2017-09-19 15:44:18 -0400

    add a single tweet route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const nunjucks = require('nunjucks');
 const express = require( 'express' );
+const morgan = require('morgan');
 const bank = require('./tweetBank');
 const app = express(); // creates an instance of an express application
 const routes = require('./routes');
@@ -9,29 +10,45 @@ app.engine('html', nunjucks.render); // when giving html files to res.render, te
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
 
-const locals = {
-  title: 'An Example',
-  people: [
-    { name: 'Gandalf'},
-    { name: 'Frodo' },
-    { name: 'Hermione'}
-  ]
-};
+// const locals = {
+//   title: 'An Example',
+//   people: [
+//     { name: 'Gandalf'},
+//     { name: 'Frodo' },
+//     { name: 'Hermione'}
+//   ]
+// };
 
+//Middleware
+app.use(morgan('combined'));
+
+//Routes
 app.use('/', routes);
-
-app.use(function (req, res, next) {
-    console.log(req.method, req.url, 'StatusCode: ', res.statusCode);
-    next();
-});
-
-app.use('/special', function (req, res, next) {
-  res.send("You're not supposed to be here bro >:(");
-  next();
-});
-
-
 
 app.listen(3000, function() {
   console.log("Listening on port ", 3000);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(function (req, res, next) {
+//     console.log(req.method, req.url, 'StatusCode: ', res.statusCode);
+//     next();
+// });
+
+// app.use('/special', function (req, res, next) {
+//   res.send("You're not supposed to be here bro >:(");
+//   next();
+// });
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ const path = require('path');
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+
+router.use(express.static('public'));
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
 
-router.use(express.static('public'));
+router.get( '/users/:name', function (req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  console.log('list', list);
+  res.render( 'index', {tweets: list} );
+});
 
 module.exports = router;
